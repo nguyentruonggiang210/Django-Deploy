d9e5e48f2d215c82de06647ff40ef23411839489
root <root@kali>
2021-05-11 10:56:43 +0700

    Updated

--- djangocoban/settings.py
+++ djangocoban/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'live-static-files','static-root')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
@@ -132,5 +133,4 @@ STATICFILES_DIRS = (
 )
 
 MEDIA_URL = "/Media/"
-MEDIA_ROOT = os.path.join(BASE_DIR,"Media")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_ROOT = os.path.join(BASE_DIR,'live-static-files',"media-root")
